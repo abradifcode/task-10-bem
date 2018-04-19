487526f49c60d636c6ba0b224e75196bffe5dedd
yokselzok <yokselzok@gmail.com>
2018-04-19 21:31:47 +0300

    Make src/blocks default for bem create

--- .bemrc.js
+++ .bemrc.js
@@ -33,7 +33,7 @@ module.exports = {
       plugins: {
         create: {
           levels: {
-            'src/desktop': { default: true }
+            'src/blocks': { default: true }
           },
           techs: ['js', 'scss'],
           templates: {
