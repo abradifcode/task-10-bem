91b7f02ced299dec2be826aeb37ed310dabc9c90
yokselzok <yokselzok@gmail.com>
2018-04-21 11:07:58 +0300

    Remove needless files

--- src/blocks/Post/Title/_size_m/Post-Title_size_m.js
+++
@@ -1,6 +0,0 @@
-import { declMod } from 'bem-react-core';
-
-export default declMod({ size_m: true }, {
-    block: 'Post',
-    elem: 'Title'
-});
--- src/blocks/Post/_size_l/Post_size_l.js
+++
@@ -1,5 +0,0 @@
-import { declMod } from 'bem-react-core';
-
-export default declMod({ size_l: true }, {
-    block: 'Post'
-});
